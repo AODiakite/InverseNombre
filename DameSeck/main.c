013074a8119cee7ea4146e4ff02d4f9fd66bf648
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2021-11-10 23:27:49 +0100

    Update main.c

--- DameSeck/main.c
+++ DameSeck/main.c
@@ -6,9 +6,25 @@
 //
 
 #include <stdio.h>
-
+#include <math.h>
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int N,temp,a;
+    int T[4];
+    
+    printf("Donnez un nombre de 4 chiffres :");
+    scanf("%d",&N);
+    a=N;
+    for (int i=3; i>=0; i--) {
+        temp=N/pow(10,i);
+        T[i]=temp;
+        N-=temp*pow(10, i);
+    }
+    printf("L'ecriture inverse du nombre %d est :\n",a);
+    for (int i=0; i<4; i++) {
+        printf("%d",T[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
