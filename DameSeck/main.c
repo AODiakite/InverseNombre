a9ba91bbda10f0b9a84dbf677f7f0f458e97b041
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2021-11-10 22:53:25 +0100

    Initial Commit

---
+++ DameSeck/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DameSeck
+//
+//  Created by Diakite on 10/11/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
